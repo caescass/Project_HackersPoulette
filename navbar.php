fd098e847bb44bfe6e627afedcd158326d5d0c7e
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-04-02 15:29:58 +0200

    Page produit première version OK - amélioration à venir

--- navbar.php
+++ navbar.php
@@ -4,7 +4,7 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <img src="/assets/img/hackers-poulette-logo_blanc-02.svg" style="height: 5em; margin-right: 3em"/>
+        <img src="assets/img/hackers-poulette-logo_blanc-02.svg" style="height: 5em; margin-right: 3em"/>
         <a class="navbar-brand footer" href="index.php">Hackers Poulette</a>
             <ul class="navbar-nav">
                 <li class="nav-item active footer">
